3fe74d92991704f9aeb58f0bae9fcd447627f715
Hunt Lin <hunt.lin@tes-tec.com>
2019-12-18 17:11:07 +0800

    A quick fix.

--- chasgimqtt/server.py
+++ chasgimqtt/server.py
@@ -173,8 +173,8 @@ class Server(object):
         logger.info("Loop for messages pool")
 
         while True:
-            logger.info("Wait for a message from channel %s", self.mqtt_channel_name)
-            self._mqtt_receive(await self.channel.receive(self.mqtt_channel_name))
+            logger.info("Wait for a message from channel %s", self.mqtt_channel_pub)
+            self._mqtt_receive(await self.channel.receive(self.mqtt_channel_pub))
 
     def stop_server(self, signum):
         logger.info("Received signal {}, terminating".format(signum))
